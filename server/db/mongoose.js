285e0fa53041f6272d2548992380b93772944821
vasujain314 <jain.vasu509@gmail.com>
2020-03-30 00:50:42 +0530

    db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,23 +1,23 @@
-// var mongoose=require('mongoose');
+var mongoose=require('mongoose');
 
-// mongoose.Promise=global.Promise;
-// mongoose.connect('mongodb://localhost:27017/Todoapp',{
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-// });
-
-
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://vasujain314:3vAGvcl4GPUXo69W@cluster0-nauop.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.Promise=global.Promise;
+mongoose.connect('mongodb+srv://vasujain314:<3vAGvcl4GP>devicesUXo69W@cluster0-nauop.mongodb.net/test?retryWrites=true&w=majority',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
 
 
 
-module.exports = {MongoClient};
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://vasujain314:3vAGvcl4GPdevicesUXo69W@cluster0-nauop.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+module.exports = {mongoose};
