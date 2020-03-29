d3da4476edde0208aa3568982ba8ad6575afe0b1
vasujain314 <jain.vasu509@gmail.com>
2020-03-29 23:52:41 +0530

    db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,23 @@
-var mongoose=require('mongoose');
+// var mongoose=require('mongoose');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/Todoapp',{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+// mongoose.Promise=global.Promise;
+// mongoose.connect('mongodb://localhost:27017/Todoapp',{
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// });
+
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://vasujain314:Jainjain@3314@cluster0-nauop.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
-module.exports = {mongoose};
+
+
+module.exports = {MongoClient};
